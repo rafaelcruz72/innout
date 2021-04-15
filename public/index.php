f657cbea646dd459f3822f7388cda92dea8c03bc
rafaelcruz72 <rafaelcruz72@hotmail.com>
2021-04-14 21:03:40 -0300

    commit end of the day 2021-04-14

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
-<h1>OK</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+//echo $user->getSelect('id, name');
+
+print_r(User::get(['id' => 1], 'name, email'));
+echo '<br>';
+
+print_r(User::get());
+echo '<br>';
+
+foreach(User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>';
+}
